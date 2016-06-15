378c0fc2a82e249c5c775bdc1d903db115e84db9
Nathan Davis <ndavis@positronic-solutions.com>
2016-06-15 14:32:45 -0500

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,25 @@
+;; Copyright 2016 Positronic Solutions, LLC.
+;;
+;; This file is part of pulley.monad.
+;;
+;; pulley.monad is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Lesser General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; pulley.monad is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with pulley.monad.  If not, see <http://www.gnu.org/licenses/>.
+
+(defproject com.positronic-solutions/pulley.monad "0.1.0-SNAPSHOT"
+  :description "A generic monad library for Clojure"
+  :url "https://github.com/positronic-solutions/pulley.monad"
+  :license {:name "GNU Lesser General Public License, version 3 or later"
+            :url "http://www.gnu.org/licenses/lgpl.html"
+            :distribution :repo}
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.8.0"]])
